2debe9c2065e501d42035d55ff4bc7eacdffa694
Yuzuru-E <yzryzr88@gmail.com>
2020-08-02 16:00:14 +0900

    DB設計テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  enum sunday: [ :off, :on ]
+  enum monday: [ :off, :on ]
+  enum tuesday: [ :off, :on ]
+  enum wednsday: [ :off, :on ]
+  enum thursday: [ :off, :on ]
+  enum friday: [ :off, :on ]
+  enum saturday: [ :off, :on ]
+  enum workoutTime: 
 end
--- db/migrate/20200623081503_create_users.rb
+++ db/migrate/20200623081503_create_users.rb
@@ -2,7 +2,14 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :line_id, null: false
-
+      t.boolean :sunday
+      t.boolean :monday
+      t.boolean :tuesday
+      t.boolean :wednesday
+      t.boolean :thursday
+      t.boolean :friday
+      t.boolean :saturday
+      t.integer :workoutTime 
       t.timestamps
     end
   end
