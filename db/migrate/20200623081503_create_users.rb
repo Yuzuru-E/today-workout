4d86ef99d62e2b8e836679ab514266f6c20c3bbd
Yuzuru-E <yzryzr88@gmail.com>
2020-08-10 11:47:18 +0900

    Update 20200623081503_create_users.rb

--- db/migrate/20200623081503_create_users.rb
+++ db/migrate/20200623081503_create_users.rb
@@ -2,13 +2,13 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :line_id, null: false
-      t.boolean :sunday
-      t.boolean :monday
-      t.boolean :tuesday
-      t.boolean :wednesday
-      t.boolean :thursday
-      t.boolean :friday
-      t.boolean :saturday
+      t.string :sunday
+      t.string :monday
+      t.string :tuesday
+      t.string :wednesday
+      t.string :thursday
+      t.string :friday
+      t.string :saturday
       t.integer :workoutTime 
       t.timestamps
     end
